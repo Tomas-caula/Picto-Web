eb92346d59162c9334add1cff905335796794fdb
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2023-06-06 11:01:51 -0300

    Starting home page

---
+++ picto/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Home">
+        <div className="left"></div>
+        <div className="middle">
+
+            <h1 id='speak'>SPEAK THROUGH PICTOS</h1>
+            <div className="big-squars">
+                <div className="text">
+                    <h1>
+                     <span>Picto</span>  is a friendly-user translator targeted to <br/> people with <br/> communication <br/> difficulties.
+                    </h1>
+                </div>
+            </div>
+            <div className="big-squars">
+                <div className="text left">
+                    <h1>
+                    A pictogram-to-text based translation application introducing over a  <br/> <span>+ 1000</span>  <br/> images. 
+                    </h1>
+                </div>
+
+            </div>
+        </div>
+        <div className="right"></div>
+    </div>
+  );
+}
+
+export default App;
