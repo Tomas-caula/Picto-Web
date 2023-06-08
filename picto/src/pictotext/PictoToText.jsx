d0d4f35b79d810e2e1d100b9d5aa8136e452b4d3
Tomas-caula <70955134+Tomas-caula@users.noreply.github.com>
2023-06-08 20:30:10 -0300

    pop up to choose

--- picto/src/pictotext/PictoToText.jsx
+++ picto/src/pictotext/PictoToText.jsx
@@ -9,7 +9,8 @@ function PictoToText() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedPictos, setSelectedPictos] = useState([]);
 
-  const [element, setElement] = useState(document.getElementById("element"))
+
+  const divRef = useRef();
 
   const element1Ref = useRef(null);
   const [showOptions, setshowOptions] = useState(false)
@@ -19,6 +20,20 @@ function PictoToText() {
   const handleCategoryClick = (subcategory) => {
     setSelectedCategory(subcategory);
   };
+
+  
+
+  function selectOne (id){
+    var elementos = document.getElementsByTagName("*");
+
+// Itera sobre cada elemento y remueve la clase deseada
+  for (var i = 0; i < elementos.length; i++) {
+    elementos[i].classList.remove("selected");
+  }
+
+    var newSelected = document.getElementById(id)
+    newSelected.classList.add("selected")
+    }
 
   const handlePictogramClick = (subcategory) => {
     if (subcategory.subcategories.length === 0) {
@@ -75,10 +90,10 @@ function PictoToText() {
       </div>
 
       {showOptions ? ( 
-          <ul style={{position: "absolute", left: positionMenu[1], top: positionMenu[0]}}>
-            <li> <img src='icons/español.png'></img> Español</li>
-            <li> <img src='icons/español.png'/> Ingles</li>
-            <li> <img src='icons/español.png'/> Pictogramas</li>
+          <ul ref={divRef} style={{position: "absolute", left: positionMenu[1], top: positionMenu[0]}}>
+            <li onClick={() => selectOne("espanol")} > <img src='icons/español.png'></img> Español <svg id='espanol' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg> </li>
+            <li onClick={() => selectOne("ingles")}> <img src='icons/ingles.png'/> Ingles <svg id='ingles' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg> </li>
+            <li onClick={() => selectOne("pictogramas")}> Pictogramas <svg id='pictogramas' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"></polyline></svg> </li>
           </ul>
       ):null
       }
